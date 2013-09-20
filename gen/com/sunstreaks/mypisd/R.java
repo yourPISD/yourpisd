ab6f8d6a50bcc6bf6e76cf8cd159a9ea6c6eaabb
Han Li <dragon1357531@gmail.com>
2013-09-19 21:04:11 -0500

    Fixed some margins and paddings. Proof of concept, will fix later. Background color improved.

--- gen/com/sunstreaks/mypisd/R.java
+++ gen/com/sunstreaks/mypisd/R.java
@@ -11,6 +11,7 @@ public final class R {
     public static final class attr {
     }
     public static final class color {
+        public static final int gray=0x7f040001;
         public static final int light_blue=0x7f040000;
     }
     public static final class dimen {
